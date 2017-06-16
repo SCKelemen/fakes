f438771a841fd8adddb4730abff3d737d5e17947
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2017-06-16 10:48:36 -0400

    DeprecatedCipherException

---
+++ exceptions.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityFramework.Cipher
+{
+    
+    public class DeprecatedCipherException : CryptographicException
+    {
+        public DeprecatedCipherException()
+        {
+            
+        }
+
+        public DeprecatedCipherException(string message)
+        {
+            
+        }
+
+        public DeprecatedCipherException(int hr)
+        {
+            
+        }
+
+        public DeprecatedCipherException(string format, string insert)
+        {
+            
+        }
+
+        public DeprecatedCipherException(string message, Exception inner)
+        {
+            
+        }
+
+    }
+}
